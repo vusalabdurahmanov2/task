575d434e90fe1231be1e0f523490c66b9b9e220b
Vusal Abdurahmanov <tu7eay65k@aztu.local>
2022-03-04 14:09:47 +0400

    initial commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n-i daxil edin: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("m-i daxil edin: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            int counter = 0;
+            Console.Write("Neciye bolmek isteyirsiniz?: ");
+            int x = Convert.ToInt32(Console.ReadLine());//Bolunen eded
+            if (n < m)
+            {
+                for (int i = n; i <= m; i++)
+                    if (i % x == 0 && i != 0)
+                        counter++;
+                       
+                if (counter == 0)
+                {
+                    Console.WriteLine("Bolunen Yoxudur");
+                }
+                else
+                {
+                    Console.WriteLine(counter);
+                }
+                
+            }
+            else if(n==m){
+                Console.WriteLine("Ededler beraberdiler ");
+            }
+            else if(n>m)
+            {            
+                for (int i = m; i <= n; i++)
+                    if (i % x == 0 && i != 0)
+                        counter++;
+                if (counter == 0)
+                {
+                    Console.WriteLine("Bolunen Yoxudur");
+                }
+                else
+                {
+                    Console.WriteLine(counter);
+                }
+            }
+
+        }
+        } 
+
+    }
---
+++ task2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string month = Console.ReadLine();
+
+            switch (month)
+            {
+                case "Yanvar":
+                case "Mart":
+                case "may":
+                case "Iyul":
+                case "Avgust":
+                case "Oktyabr":
+                case "Dekabr":
+                    Console.WriteLine(31);
+                        break;
+                case "Aprel":
+                case "Iyun":
+                case "Sentyabr":
+                case "Noyabr":
+                    Console.WriteLine(30);
+                    break;
+                case "Fevral":
+                    Console.WriteLine(28);
+                    break;
+
+                default:
+                    Console.WriteLine("Bele Bir ay yoxdur");
+                    break;
+
+
+
+            }
+
+        }
+    }
+}
